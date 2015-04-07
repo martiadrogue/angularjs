f20f070dd7ad0e35fc2f3aaef78010d7c4c09fe3
marti adrogue <marti.adrogue@gmail.com>
2015-04-07 13:57:03 +0200

    Change Object's array to data of JSON.

--- part3/js/main.js
+++ part3/js/main.js
@@ -2,9 +2,13 @@ var app = angular.module('app', []);
 app.controller('SettingsController', ['$scope', '$http', SettingsController]);
 
 function SettingsController($scope, $http) {
-  $http.get('js/data.json').success(fillArtists);
+  $http.get('js/data.json').success(fillArtists).error(showError);
 
   function fillArtists (data) {
     $scope.artists = data;
+  }
+
+  function showError () {
+    alert('Something is wrong with that request! ');
   }
 }
