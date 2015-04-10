4d37b4cadba4103088260ca5832f300a330f53b7
marti adrogue <marti.adrogue@gmail.com>
2015-04-10 12:52:43 +0200

    Init stage for part 7.

---
+++ part7/js/main.js
@@ -0,0 +1,15 @@
+var artistControllers = angular.module('artistControllers', []);
+artistControllers.controller('ListController', ['$scope', '$http', ListController]);
+
+function ListController($scope, $http) {
+  $http.get('js/data.json').success(fillArtists).error(showError);
+
+  function fillArtists (data) {
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  }
+
+  function showError () {
+    alert('Something is wrong with that request! ');
+  }
+}
