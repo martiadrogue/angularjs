cec361ee2a6d2e92e27625b7532967732ef9adf6
marti adrogue <marti.adrogue@gmail.com>
2015-04-13 11:28:57 +0200

    Declare nextItem and prevItem variables.

--- part10/js/controllers.js
+++ part10/js/controllers.js
@@ -22,6 +22,18 @@ function DetailsController($scope, $http, $routeParams) {
   function fillArtists (data) {
     $scope.artists = data;
     $scope.whichItem = $routeParams.itemId;
+
+    if ($routeParams.itemId > 0) {
+      $scope.prevItem = Number($routeParams.itemId) - 1;
+    } else {
+      $scope.prevItem = $scope.artists.length - 1;
+    }
+
+    if ($routeParams.itemId < $scope.artists.length - 1) {
+      $scope.nextItem = Number($routeParams.itemId) + 1;
+    } else {
+      $scope.nextItem = 0;
+    }
   }
 
   function showError () {
